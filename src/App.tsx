8cda79c15a85822f27944d2c49ef16b8e59d3b40
Svitlana <devop.edu@gmail.com>
2021-11-30 14:21:36 +0100

    fetched data with type

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,44 @@
-import React from 'react';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import Card, { CardVariant } from "./components/Card";
-import UserList from './components/UserList';
-import {IUser} from './types/types'
+import UserList from "./components/UserList";
+import { IUser } from "./types/types";
 
-const App: React.FC = () =>{
+const App: React.FC = () => {
+  const [users, setUsers] = useState<IUser[]>([]);
+  // ex
+  const user: IUser[] = [
+    {
+      id: 1,
+      name: "Bob",
+      email: "bob@gmail.com",
+      address: { city: "Lund", street: "Kallbyvagen 3C", zipcode: "225 67" },
+    },
+    {
+      id: 2,
+      name: "Olga",
+      email: "olga@gmail.com",
+      address: { city: "Malmo", street: "Lundvagen 35G", zipcode: "123 456" },
+    },
+  ];
 
-  const users: IUser[] =[
-    {id: 1, name: 'Bob', email: 'bob@gmail.com', address:{city: 'Lund', street:  'Kallbyvagen 3C', zipcode: '225 67'}},
-    {id: 2, name: 'Olga', email: 'olga@gmail.com', address:{city: 'Malmo', street:  'Lundvagen 35G', zipcode: '123 456'}}
-  ]
-  const handleClick = (num: number) => {
-    console.log('clicked, set from child', num )
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  async function fetchUsers() {
+    try {
+      const response = await axios.get<IUser[]>(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      setUsers(response.data);
+    } catch (e) {
+      console.log("The error has been occured", e);
+    }
   }
+  const handleClick = (num: number) => {
+    console.log("clicked, set from child", num);
+  };
   return (
     <div>
       <Card
@@ -23,9 +50,9 @@ const App: React.FC = () =>{
         <button>Works</button>
       </Card>
 
-      <UserList users={users}/>
+      <UserList users={users} />
     </div>
   );
-}
+};
 
 export default App;
